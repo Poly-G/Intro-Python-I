5a719ad4b8453b53ded6c05aec3cd783d84d5d33
Poly Goveia <poly.goveia@gmail.com>
2020-05-11 18:51:38 -0700

    add 14

--- src/14_cal.py
+++ src/14_cal.py
@@ -4,7 +4,7 @@ render a calendar to your terminal.
 https://docs.python.org/3.6/library/calendar.html
 
 Write a program that accepts user input of the form
-  `14_cal.py [month] [year]`
+  `14_cal.py month [year]`
 and does the following:
  - If the user doesn't specify any input, your program should
    print the calendar for the current month. The 'datetime'
@@ -17,16 +17,28 @@ and does the following:
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
    Then exit the program.
-
-Note: the user should provide argument input (in the initial call to run the file) and not 
-prompted input. Also, the brackets around year are to denote that the argument is
-optional, as this is a common convention in documentation.
-
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
-it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+def calendar_output():
+  def print_cal(month=datetime.now().month, year=datetime.now().year):
+    print(calendar.TextCalendar().formatmonth(int(year), int(month)))
+
+  # if 0 arg 
+  if len(sys.argv) == 1: 
+    print_cal()
+  # if 1 arg
+  elif len(sys.argv) == 2:
+    print_cal(sys.argv[1])
+  # if 2 args
+  elif len(sys.argv) == 3: 
+    print_cal(sys.argv[1], sys.argv[2][1:-1])
+  # otherwise if > 2 args
+  else:
+    print("\n***\nIncorrect usage, please use command form <calendar.py month [year]>\n***")
+
+calendar_output()
+  
